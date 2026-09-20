37f5b8c0bac82b2454e10d00571a449337420371
uruz8926 <n069001246@gmail.com>
2026-09-20 22:04:29 +0900

    Update mini_trackpad_iqs7211e_init.h

--- src/mini_trackpad_iqs7211e_init.h
+++ src/mini_trackpad_iqs7211e_init.h
@@ -74,8 +74,8 @@
 
 /* Thresholds and Debounce Settings */
 /* Memory Map Position 0x38 - 0x3A */
-#define TRACKPAD_TOUCH_SET_THRESHOLD             0x0E
-#define TRACKPAD_TOUCH_CLEAR_THRESHOLD           0x0A
+#define TRACKPAD_TOUCH_SET_THRESHOLD             0x0C
+#define TRACKPAD_TOUCH_CLEAR_THRESHOLD           0x08
 #define ALP_THRESHOLD_0                          0x08
 #define ALP_THRESHOLD_1                          0x00
 #define ALP_SET_DEBOUNCE                         0x04
